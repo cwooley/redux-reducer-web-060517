fbf5cfdba8778bf5ddfd7df8b7c4dbaa02870d10
Charles Wooley <cwooley89@gmail.com>
2017-08-24 11:49:46 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case "INCREASE":
+      let increasedPresents = state.numberOfPresents + 1
+      return {numberOfPresents: increasedPresents}
+      break;
+    case "DECREASE":
+      let decreasedPresents = state.numberOfPresents - 1
+      return {numberOfPresents: decreasedPresents}
+      break;
+    default:
+      return state
+
+  }
 }
